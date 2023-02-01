2d9a77a7257499b92a237852d92d3600ee5ff11e
FX <75465898+Frans-Xaverius@users.noreply.github.com>
2023-02-01 16:17:09 +0800

    Generate SPPH Doc

--- resources/views/module/procurement/export_spph_tor_pdf.blade.php
+++ resources/views/module/procurement/export_spph_tor_pdf.blade.php
@@ -1,3 +1,26 @@
+<?php 
+    
+    function formatDate ($tanggal){
+        $bulan = array (
+            1 =>   'Januari',
+            'Februari',
+            'Maret',
+            'April',
+            'Mei',
+            'Juni',
+            'Juli',
+            'Agustus',
+            'September',
+            'Oktober',
+            'November',
+            'Desember'
+        );
+        $field = explode('-', $tanggal);
+        return $field[2] . ' ' . $bulan[ (int)$field[1] ] . ' ' . $field[0];
+    }
+
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -10,23 +33,28 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
     </head>
+    <style type="text/css">
+        .table td, .table th {
+            border: 1px solid;
+        }
+    </style>
     <body style="margin-left:20px;font: normal 14px, Times New Roman, Times, serif;">
         <center>
             <img class="mt-2 mb-3" src="{{public_path('img/up.png')}}" width="20%"><br> 
         </center>
-        Jakarta, {{date('d/m/Y')}}<br><br>
+        Jakarta, {{formatDate(date('Y-m-d'))}}<br><br>
         <table style="margin-left:10px;">
             @if($procurement->mechanism_id!=6)
                 <tr>
                     <td style="width:120px;">Nomor</td>
                     <td style="width:2px;">:</td>
-                    <td style="width:250px;">{{$spph->no_spph}}</td>
+                    <td style="width:350px;">{{$spph->no_spph}}</td>
                 </tr>
             @endif
             <tr>
                 <td>Perihal</td>
                 <td>:</td>
-                <td style="width:200px;"><b>Permintaan Penawaran Harga</b>
+                <td style="width:300px;"><b>Permintaan Penawaran Harga</b>
                 @if($procurement->mechanism_id!=6) 
                     <b>{{$spph->procurement->name}}</b>
                 @else 
@@ -42,13 +70,14 @@
         <br><br>
         Dengan hormat, <br>
         <p align="justify">
-        Berkaitan dengan memorandum nomor @if($procurement->mechanism_id!=6) {{$spph->procurement->no_memo}} @else {{$procurement->no_memo}} @endif dari Direktur Penegelolaan Sistem dan
-        Teknologi Informasi, mengacu pada disposisi dengan nomor agenda 0491 tanggal 01-April-2021, dari Wakil
-        Rekto II Bidang Keuangan dan Sumber Daya Organisasi, disposisi nomor 0079 tanggal 5-04-2021, dari Direktur
-        Pengelolaan Fasilitas Universitas tentang Pengadaan Data Center Pendidikan dan Penelitian Universitas
-        Pertamina kami dari Fungsi Pengadaan Barang dan Jasa Universitas Pertamina mengundang perusahaan
+        Berkaitan dengan memorandum nomor @if($procurement->mechanism_id!=6) {{$spph->procurement->no_memo}} @else {{$procurement->no_memo}} @endif tentang {{$spph->procurement->name}} dari 
+        @foreach ($disposisi as $key=>$dispo)
+            {{ $dispo['nama_jabatan'] }} pada tanggal {{ formatDate($dispo['tgl_disposisi']) }},
+            @if ($key+1 == sizeof($disposisi)-1) serta @endif
+        @endforeach
+        kami dari Fungsi Pengadaan Barang dan Jasa Universitas Pertamina mengundang perusahaan
         Bapak/Ibu untuk mengajukan penawaran terkait pengadaan tersebut. Spesifikasi kebutuhan dapat dilihat pada
-        Lampiran.</p>
+        Lampiran.
         Berikut adalah penjelasan singkat dalam pengajuan permohonan penawaran tersebut:<br>
         1. Perusahaan yang telah diundang dipersilahkan untuk melampirkan penawaran harga dengan syarat sebagai
         berikut: <br>
